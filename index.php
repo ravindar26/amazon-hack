b82941ddd95f171212b6833f87db6312a7845bb0
ravindar26 <bravindarkumar26@gmail.com>
2014-12-20 22:34:58 +0530

    added index page

--- index.php
+++ index.php
@@ -1,14 +1,18 @@
 <?php
 require_once "connect.php";
+
 session_start();
 if(!isset($_POST['login'])){
 ?>
 <!DOCTYPE html>
 <html>
 <head>
+	<script type="type/javascript" src="post.js"></script>
 	<title>Status</title>
+		
 </head>
 <body>
+	<a href="#" onclick="check()">clickhere</a>
 	<form action="index.php" method="post">
 	<table>
 		<tr>
@@ -28,11 +32,10 @@ if(!isset($_POST['login'])){
 		</tr>
 	</table>
 	</form>	
-</body>
-</html>
+
+
 <?php
 }else{
-	$_SESSION['username'] = $_POST['uname'];
 	function keymaker($password){
 		$key = 'sxwrsxetdcbugynhmjoiokjimhuyntrbdcesxwe5dcr6vft7bgy8hnu9mjiko';
 		$k = md5($password.$key);
@@ -44,9 +47,23 @@ if(!isset($_POST['login'])){
 	
 	if(mysql_num_rows($query)){
 		echo "<center>YOu have logged on successfully. Tabler under Work <h2>Get Lost</h2></center>";
+
+	$_SESSION['username'] = $_POST['uname'];
+	echo "hi, ".$_SESSION['username'];
+	echo "<H1>Ideas</H1>";
+	echo "<div id='ideas' style='height:75px;width:240px;background-color:yellow;border:1px;'>";
+	echo "<form name='from1'>";
+	echo "<textarea name='msg' style='height:50px;width:200px;'></textarea>";
+	echo "</form1>";
+	echo "<span style='margin-left:150px;'><a href='#' onclick='postmsg()'>post</a></span>";
+	echo "</div>";
+	echo "<div id='post' style='background-color:red;width:240px;height:500px;'> loading posts....</div>";	
 	}else{
 		echo "<center>Invalid username or password. Click <a href='index.php'>here</a> to Login Again</center>";
 		//unset($_SESSION['username']);
 	}
 }
+
 ?>
+</body>
+</html>
